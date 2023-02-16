8a0a14ba26b80c470b2be98b2c06ee8b36612215
jeffny2015 <jeffny2015@gmail.com>
2023-02-16 16:17:01 -0600

    fix

--- src/api/cat.py
+++ src/api/cat.py
@@ -13,8 +13,6 @@ router = APIRouter(
 
 @router.get("", response_model=CatResponse, status_code=HTTP_200_OK)
 async def get_cats(settings: APICatSettings = Depends(get_cat_settings), api_key: APIKey = Depends(auth.get_api_key)):
-    print("hola")
-    print(api_key)
     async with AsyncClient() as client:
         response = await client.get(url=settings.API_URI)
         r = response.json()
